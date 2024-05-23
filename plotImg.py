eb7d25c8279eb86ee774144c670eed1ef528d45c
Eric-IIP <s2465062@u.tsukuba.ac.jp>
2024-05-23 12:11:19 +0900

    modified the dice calculation method added edited the plot function

--- plotImg.py
+++ plotImg.py
@@ -6,14 +6,15 @@ def plot_images(images, titles=None):
     num_cols = 3  # 3 columns
     num_rows = (num_images - 1) // num_cols + 1
 
-    fig, axes = plt.subplots(num_rows, num_cols, figsize=(0.5, 0.5))
+    fig, axes = plt.subplots(num_rows, num_cols, figsize=(8, 8))
+
+
+
     axes = axes.flatten()
 
     for i, (image, ax) in enumerate(zip(images, axes)):
         ax.imshow(image)
         ax.axis('off')
-        if titles is not None:
-            ax.set_title(titles[i])
 
     # Hide any remaining empty subplots
     for ax in axes[num_images:]:
@@ -25,6 +26,5 @@ def plot_images(images, titles=None):
 # Example usage:
 # Assuming 'images' is a list containing your NumPy arrays
 # And 'titles' is a list of titles for each image (optional)
-images = [np.random.random((100, 100)) for _ in range(9)]  # Example images
-titles = ['Image {}'.format(i+1) for i in range(len(images))]  # Example titles
-plot_images(images, titles)
+#images = [np.random.random((100, 100)) for _ in range(9)]  # Example images
+#plot_images(images, titles)
