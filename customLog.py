6f94672360ee6cda1feeb3911c5f3f37491e5b8e
Eric-IIP <s2465062@u.tsukuba.ac.jp>
2024-05-24 18:09:47 +0900

    Ignore checkpoint file

---
+++ customLog.py
@@ -0,0 +1,12 @@
+import os
+
+def custom_logger(*args, log_file):
+    directory = os.path.dirname(log_file)
+    if not os.path.exists(directory):
+        os.makedirs(directory)  # Create directory if it doesn't exist
+
+    with open(log_file, "a") as f:
+        if os.path.exists(log_file):
+            f.write("\n")  # Start from a new line if the file exists
+        for var in args:
+            f.write(f"{var}: {eval(var)}\n")
