fcf68c5584ec3d67658cbbbde10cdb57d94b5f45
Eric-IIP <s2465062@u.tsukuba.ac.jp>
2024-06-11 16:19:10 +0900

    experimenting, doing cross validation

--- automation.py
+++ automation.py
@@ -10,10 +10,22 @@ import tempfile
 
 
 parameter_sets = [
-    {'N_BLOCK': 4, 'LR': 0.01,  'OUTPUT_DIR': 'o5'},
-    {'N_BLOCK': 4, 'LR': 0.001, 'OUTPUT_DIR': 'o6'},
-    {'N_BLOCK': 5, 'LR': 0.01,  'OUTPUT_DIR': 'o7'},
-    {'N_BLOCK': 5, 'LR': 0.001, 'OUTPUT_DIR': 'o8'},
+    # {'N_BLOCK': 5, 'LR': 0.01,  'OUTPUT_DIR': '0-a', 'INPUT': 'N1-', 'ANNEALING': 'N2-', 'VALIDATION': 'N3-', 'TEST': 'N4-'},
+    # {'N_BLOCK': 5, 'LR': 0.01,  'OUTPUT_DIR': '0-b', 'INPUT': 'N1-', 'ANNEALING': 'N3-', 'VALIDATION': 'N2-', 'TEST': 'N4-'},
+    # {'N_BLOCK': 5, 'LR': 0.01,  'OUTPUT_DIR': '0-c', 'INPUT': 'N2-', 'ANNEALING': 'N3-', 'VALIDATION': 'N1-', 'TEST': 'N4-'},
+    
+    # {'N_BLOCK': 5, 'LR': 0.01,  'OUTPUT_DIR': '1-a', 'INPUT': 'N1-', 'ANNEALING': 'N2-', 'VALIDATION': 'N4-', 'TEST': 'N3-'},
+    # {'N_BLOCK': 5, 'LR': 0.01,  'OUTPUT_DIR': '1-b', 'INPUT': 'N1-', 'ANNEALING': 'N4-', 'VALIDATION': 'N2-', 'TEST': 'N3-'},
+    # {'N_BLOCK': 5, 'LR': 0.01,  'OUTPUT_DIR': '1-c', 'INPUT': 'N2-', 'ANNEALING': 'N4-', 'VALIDATION': 'N1-', 'TEST': 'N3-'},
+
+    {'N_BLOCK': 5, 'LR': 0.01,  'OUTPUT_DIR': '2-a', 'INPUT': 'N1-', 'ANNEALING': 'N3-', 'VALIDATION': 'N4-', 'TEST': 'N2-'},
+    {'N_BLOCK': 5, 'LR': 0.01,  'OUTPUT_DIR': '2-b', 'INPUT': 'N1-', 'ANNEALING': 'N4-', 'VALIDATION': 'N3-', 'TEST': 'N2-'},
+    {'N_BLOCK': 5, 'LR': 0.01,  'OUTPUT_DIR': '2-c', 'INPUT': 'N3-', 'ANNEALING': 'N4-', 'VALIDATION': 'N1-', 'TEST': 'N2-'},
+
+    # {'N_BLOCK': 5, 'LR': 0.01,  'OUTPUT_DIR': '3-a', 'INPUT': 'N2-', 'ANNEALING': 'N3-', 'VALIDATION': 'N4-', 'TEST': 'N1-'},
+    # {'N_BLOCK': 5, 'LR': 0.01,  'OUTPUT_DIR': '3-b', 'INPUT': 'N2-', 'ANNEALING': 'N4-', 'VALIDATION': 'N3-', 'TEST': 'N1-'},
+    # {'N_BLOCK': 5, 'LR': 0.01,  'OUTPUT_DIR': '3-c', 'INPUT': 'N3-', 'ANNEALING': 'N4-', 'VALIDATION': 'N2-', 'TEST': 'N1-'},
+
     # Add more parameter sets as needed
 ]
 
@@ -29,7 +41,7 @@ for params in parameter_sets:
     # Create a temporary file for the output notebook
     with tempfile.NamedTemporaryFile(suffix=".ipynb") as temp_output:
         pm.execute_notebook(
-            'mainUNet.ipynb',  # Path to your input notebook
+            'Main_without_Annealing.ipynb',  # Path to your input notebook
             temp_output.name,  # Path to the temporary output notebook
             parameters=params
         )
