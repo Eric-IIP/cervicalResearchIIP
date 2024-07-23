207ea0004cef386cee12e894ea51b54044a9d8a7
Eric-IIP <s2465062@u.tsukuba.ac.jp>
2024-07-23 18:09:50 +0900

    added a new notebook for feature generation and augmentation

--- automation.py
+++ automation.py
@@ -56,21 +56,41 @@ parameter_sets = [
     # 1-12 UNet#
     # 13-24 UNet
     # N1,2 and half of N3 training, N3 half val, N4 test
-    {'N_BLOCK': 4, 'LR': 0.01, 'IN_CHANNEL': 1,  'OUTPUT_DIR': '13', 'INPUT': ['N1', 'N2'], 'ANNEALING': [], 'VALIDATION': ['N3'], 'TEST': ['N4']},
-    {'N_BLOCK': 4, 'LR': 0.01, 'IN_CHANNEL': 1,  'OUTPUT_DIR': '14', 'INPUT': ['N1', 'N3'], 'ANNEALING': [], 'VALIDATION': ['N2'], 'TEST': ['N4']},
-    {'N_BLOCK': 4, 'LR': 0.01, 'IN_CHANNEL': 1,  'OUTPUT_DIR': '15', 'INPUT': ['N2', 'N3'], 'ANNEALING': [], 'VALIDATION': ['N1'], 'TEST': ['N4']},
+    # {'N_BLOCK': 4, 'LR': 0.01, 'IN_CHANNEL': 45,  'OUTPUT_DIR': '13', 'INPUT': ['N1', 'N2','N3-1-', 'N3-2-', 'N3-3-', 'N3-4-', 'N3-5-'], 'ANNEALING': [], 'VALIDATION': ['N3-6-', 'N3-7-', 'N3-8-', 'N3-9-', 'N3-10-'], 'TEST': ['oN4']},
+    # {'N_BLOCK': 4, 'LR': 0.01, 'IN_CHANNEL': 45,  'OUTPUT_DIR': '14', 'INPUT': ['N1', 'N3', 'N2-1-', 'N2-2-', 'N2-3-', 'N2-4-', 'N2-5-'], 'ANNEALING': [], 'VALIDATION': ['N2-6-', 'N2-7-', 'N2-8-', 'N2-9-', 'N2-10-'], 'TEST': ['oN4']},
+    # {'N_BLOCK': 4, 'LR': 0.01, 'IN_CHANNEL': 45,  'OUTPUT_DIR': '15', 'INPUT': ['N2', 'N3', 'N1-1-', 'N1-2-', 'N1-3-', 'N1-4-', 'N1-5-'], 'ANNEALING': [], 'VALIDATION': ['N1-6-', 'N1-7-', 'N1-8-', 'N1-9-', 'N1-10-'], 'TEST': ['oN4']},
     
-    {'N_BLOCK': 4, 'LR': 0.01, 'IN_CHANNEL': 1,  'OUTPUT_DIR': '16', 'INPUT': ['N1', 'N2'], 'ANNEALING': [], 'VALIDATION': ['N4'], 'TEST': ['N3']},
-    {'N_BLOCK': 4, 'LR': 0.01, 'IN_CHANNEL': 1,  'OUTPUT_DIR': '17', 'INPUT': ['N1', 'N4'], 'ANNEALING': [], 'VALIDATION': ['N2'], 'TEST': ['N3']},
-    {'N_BLOCK': 4, 'LR': 0.01, 'IN_CHANNEL': 1,  'OUTPUT_DIR': '18', 'INPUT': ['N2', 'N4'], 'ANNEALING': [], 'VALIDATION': ['N1'], 'TEST': ['N3']},
+    # {'N_BLOCK': 4, 'LR': 0.01, 'IN_CHANNEL': 45,  'OUTPUT_DIR': '16', 'INPUT': ['N1', 'N2', 'N4-1-', 'N4-2-', 'N4-3-', 'N4-4-', 'N4-5-'], 'ANNEALING': [], 'VALIDATION': ['N4-6-', 'N4-7-', 'N4-8-', 'N4-9-'], 'TEST': ['oN3']},
+    # {'N_BLOCK': 4, 'LR': 0.01, 'IN_CHANNEL': 45,  'OUTPUT_DIR': '17', 'INPUT': ['N1', 'N4', 'N2-1-', 'N2-2-', 'N2-3-', 'N2-4-', 'N2-5-'], 'ANNEALING': [], 'VALIDATION': ['N2-6-', 'N2-7-', 'N2-8-', 'N2-9-', 'N2-10-'], 'TEST': ['oN3']},
+    # {'N_BLOCK': 4, 'LR': 0.01, 'IN_CHANNEL': 45,  'OUTPUT_DIR': '18', 'INPUT': ['N2', 'N4', 'N1-1-', 'N1-2-', 'N1-3-', 'N1-4-', 'N1-5-'], 'ANNEALING': [], 'VALIDATION': ['N1-6-', 'N1-7-', 'N1-8-', 'N1-9-', 'N1-10-'], 'TEST': ['oN3']},
     
-    {'N_BLOCK': 4, 'LR': 0.01, 'IN_CHANNEL': 1,  'OUTPUT_DIR': '19', 'INPUT': ['N1', 'N3'], 'ANNEALING': [], 'VALIDATION': ['N4'], 'TEST': ['N2']},
-    {'N_BLOCK': 4, 'LR': 0.01, 'IN_CHANNEL': 1,  'OUTPUT_DIR': '20', 'INPUT': ['N1', 'N4'], 'ANNEALING': [], 'VALIDATION': ['N3'], 'TEST': ['N2']},
-    {'N_BLOCK': 4, 'LR': 0.01, 'IN_CHANNEL': 1,  'OUTPUT_DIR': '21', 'INPUT': ['N3', 'N4'], 'ANNEALING': [], 'VALIDATION': ['N1'], 'TEST': ['N2']},
+    # {'N_BLOCK': 4, 'LR': 0.01, 'IN_CHANNEL': 45,  'OUTPUT_DIR': '19', 'INPUT': ['N1', 'N3', 'N4-1-', 'N4-2-', 'N4-3-', 'N4-4-', 'N4-5-'], 'ANNEALING': [], 'VALIDATION': ['N4-6-', 'N4-7-', 'N4-8-', 'N4-9-'], 'TEST': ['oN2']},
+    # {'N_BLOCK': 4, 'LR': 0.01, 'IN_CHANNEL': 45,  'OUTPUT_DIR': '20', 'INPUT': ['N1', 'N4', 'N3-1-', 'N3-2-', 'N3-3-', 'N3-4-', 'N3-5-'], 'ANNEALING': [], 'VALIDATION': ['N3-6-', 'N3-7-', 'N3-8-', 'N3-9-', 'N3-10-'], 'TEST': ['oN2']},
+    # {'N_BLOCK': 4, 'LR': 0.01, 'IN_CHANNEL': 45,  'OUTPUT_DIR': '21', 'INPUT': ['N3', 'N4', 'N1-1-', 'N1-2-', 'N1-3-', 'N1-4-', 'N1-5-'], 'ANNEALING': [], 'VALIDATION': ['N1-6-', 'N1-7-', 'N1-8-', 'N1-9-', 'N1-10-'], 'TEST': ['oN2']},
     
-    {'N_BLOCK': 4, 'LR': 0.01, 'IN_CHANNEL': 1,  'OUTPUT_DIR': '22', 'INPUT': ['N2', 'N3'], 'ANNEALING': [], 'VALIDATION': ['N4'], 'TEST': ['N1']},
-    {'N_BLOCK': 4, 'LR': 0.01, 'IN_CHANNEL': 1,  'OUTPUT_DIR': '23', 'INPUT': ['N2', 'N4'], 'ANNEALING': [], 'VALIDATION': ['N3'], 'TEST': ['N1']},
-    {'N_BLOCK': 4, 'LR': 0.01, 'IN_CHANNEL': 1,  'OUTPUT_DIR': '24', 'INPUT': ['N3', 'N4'], 'ANNEALING': [], 'VALIDATION': ['N2'], 'TEST': ['N1']},
+    # {'N_BLOCK': 4, 'LR': 0.01, 'IN_CHANNEL': 45,  'OUTPUT_DIR': '22', 'INPUT': ['N2', 'N3', 'N4-1-', 'N4-2-', 'N4-3-', 'N4-4-', 'N4-5-'], 'ANNEALING': [], 'VALIDATION': ['N4-6-', 'N4-7-', 'N4-8-', 'N4-9-'], 'TEST': ['oN1']},
+    # {'N_BLOCK': 4, 'LR': 0.01, 'IN_CHANNEL': 45,  'OUTPUT_DIR': '23', 'INPUT': ['N2', 'N4', 'N3-1-', 'N3-2-', 'N3-3-', 'N3-4-', 'N3-5-'], 'ANNEALING': [], 'VALIDATION': ['N3-6-', 'N3-7-', 'N3-8-', 'N3-9-', 'N3-10-'], 'TEST': ['oN1']},
+    # {'N_BLOCK': 4, 'LR': 0.01, 'IN_CHANNEL': 45,  'OUTPUT_DIR': '24', 'INPUT': ['N3', 'N4', 'N2-1-', 'N2-2-', 'N2-3-', 'N2-4-', 'N2-5-'], 'ANNEALING': [], 'VALIDATION': ['N2-6-', 'N2-7-', 'N2-8-', 'N2-9-', 'N2-10-'], 'TEST': ['oN1']},
+    
+    
+    
+    # {'N_BLOCK': 4, 'LR': 0.01, 'IN_CHANNEL': 108,  'OUTPUT_DIR': '13', 'INPUT': ['N1', 'N2'], 'ANNEALING': [], 'VALIDATION': ['N3'], 'TEST': ['oN4']},
+    # {'N_BLOCK': 4, 'LR': 0.01, 'IN_CHANNEL': 108,  'OUTPUT_DIR': '14', 'INPUT': ['N1', 'N3'], 'ANNEALING': [], 'VALIDATION': ['N2'], 'TEST': ['oN4']},
+    # {'N_BLOCK': 4, 'LR': 0.01, 'IN_CHANNEL': 108,  'OUTPUT_DIR': '15', 'INPUT': ['N2', 'N3'], 'ANNEALING': [], 'VALIDATION': ['N1'], 'TEST': ['oN4']},
+    
+    # {'N_BLOCK': 4, 'LR': 0.01, 'IN_CHANNEL': 108,  'OUTPUT_DIR': '16', 'INPUT': ['N1', 'N2'], 'ANNEALING': [], 'VALIDATION': ['N4'], 'TEST': ['oN3']},
+    # {'N_BLOCK': 4, 'LR': 0.01, 'IN_CHANNEL': 108,  'OUTPUT_DIR': '17', 'INPUT': ['N1', 'N4'], 'ANNEALING': [], 'VALIDATION': ['N2'], 'TEST': ['oN3']},
+    # {'N_BLOCK': 4, 'LR': 0.01, 'IN_CHANNEL': 108,  'OUTPUT_DIR': '18', 'INPUT': ['N2', 'N4'], 'ANNEALING': [], 'VALIDATION': ['N1'], 'TEST': ['oN3']},
+    
+    #{'N_BLOCK': 4, 'LR': 0.01, 'IN_CHANNEL': 108,  'OUTPUT_DIR': '19', 'INPUT': ['N1', 'N3'], 'ANNEALING': [], 'VALIDATION': ['N4'], 'TEST': ['oN2']},
+    {'N_BLOCK': 4, 'LR': 0.01, 'IN_CHANNEL': 108,  'OUTPUT_DIR': '20', 'INPUT': ['N1', 'N4'], 'ANNEALING': [], 'VALIDATION': ['N3'], 'TEST': ['oN2']},
+    {'N_BLOCK': 4, 'LR': 0.01, 'IN_CHANNEL': 108,  'OUTPUT_DIR': '21', 'INPUT': ['N3', 'N4'], 'ANNEALING': [], 'VALIDATION': ['N1'], 'TEST': ['oN2']},
+    
+    {'N_BLOCK': 4, 'LR': 0.01, 'IN_CHANNEL': 108,  'OUTPUT_DIR': '22', 'INPUT': ['N2', 'N3'], 'ANNEALING': [], 'VALIDATION': ['N4'], 'TEST': ['oN1']},
+    {'N_BLOCK': 4, 'LR': 0.01, 'IN_CHANNEL': 108,  'OUTPUT_DIR': '23', 'INPUT': ['N2', 'N4'], 'ANNEALING': [], 'VALIDATION': ['N3'], 'TEST': ['oN1']},
+    {'N_BLOCK': 4, 'LR': 0.01, 'IN_CHANNEL': 108,  'OUTPUT_DIR': '24', 'INPUT': ['N3', 'N4'], 'ANNEALING': [], 'VALIDATION': ['N2'], 'TEST': ['oN1']},
+    
+    
     # Add more parameter sets as needed
 ]
 
@@ -86,7 +106,7 @@ for params in parameter_sets:
     # Create a temporary file for the output notebook
     with tempfile.NamedTemporaryFile(suffix=".ipynb") as temp_output:
         pm.execute_notebook(
-            'mainUNet.ipynb',  # Path to your input notebook
+            'Main_without_Annealing.ipynb',  # Path to your input notebook
             temp_output.name,  # Path to the temporary output notebook
             parameters=params
         )
