c9a3e3ffaa4f0449a9d48ff07105200ce6e87665
Eric-IIP <s2465062@u.tsukuba.ac.jp>
2024-12-25 18:55:41 +0900

    Preparation 1 refactor for OPtuna MCTransunet

--- LuoTransUnet/getTransUnet.py
+++ LuoTransUnet/getTransUnet.py
@@ -5,17 +5,24 @@ import torch.nn.functional as F
 from LuoTransUnet.vit_seg_modeling import VisionTransformer as ViT_seg
 from LuoTransUnet.vit_seg_modeling import CONFIGS as CONFIGS_ViT_seg
 
-def get_transNet(n_classes, bMask = False):
+def get_transNet(n_classes, bMask = False, hidden_size = 768, num_layers = 12, decoder_channels = (256, 128, 64, 16), patch_size = 16, kernel_size: int = 1, kernel_out_channels: int = 1):
     img_size = 256
     vit_patches_size = 16
     vit_name = 'R50-ViT-B_16'
 
+    #refactored for optuna eric
     config_vit = CONFIGS_ViT_seg[vit_name]
     config_vit.n_classes = n_classes
+    config_vit.hidden_size = hidden_size
+    config_vit.transformer.num_layers = num_layers
+    config_vit.decoder_channels = decoder_channels
+    config_vit.patch_size = patch_size
+    
+    
     config_vit.n_skip = 3
     if vit_name.find('R50') != -1:
         config_vit.patches.grid = (int(img_size / vit_patches_size), int(img_size / vit_patches_size))
-    net = ViT_seg(config_vit, img_size=img_size, num_classes=n_classes, bMask=bMask)
+    net = ViT_seg(config_vit, img_size=img_size, num_classes=n_classes, bMask=bMask, kernel_size=kernel_size, kernel_out_channels=kernel_out_channels)
     return net          
 
 
